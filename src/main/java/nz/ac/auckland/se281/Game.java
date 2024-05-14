da165e1bd2555369fb80d767e72e99247736264d
Miles Makino-Torrington <mmak111@aucklanduni.ac.nz>
2024-05-15 00:27:51 +1200

    Added a message displaying name and number of fingers

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,10 +6,12 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   private int round;
+  private String[] options;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    this.options = options;
     round = 0;
   }
 
@@ -19,6 +21,7 @@ public class Game {
     MessageCli.ASK_INPUT.printMessage();
     String input = Utils.scanner.nextLine();
     MessageCli.INVALID_INPUT.printMessage();
+    MessageCli.PRINT_INFO_HAND.printMessage(options[0], input);
   }
 
   public void endGame() {}
