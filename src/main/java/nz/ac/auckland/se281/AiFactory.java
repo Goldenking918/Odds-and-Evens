b2d7c30d20ed2d6bd76112cc690cd608968478eb
Miles Makino-Torrington <mmak111@aucklanduni.ac.nz>
2024-05-16 18:04:42 +1200

    Added JavaDoc for createAi in AiFactory Class

--- src/main/java/nz/ac/auckland/se281/AiFactory.java
+++ src/main/java/nz/ac/auckland/se281/AiFactory.java
@@ -3,6 +3,12 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class AiFactory {
+  /**
+   * Creates different Ai based on the difficulty inputted.
+   *
+   * @param difficulty
+   * @return
+   */
   public static Ai createAi(Difficulty difficulty) {
     switch (difficulty) {
       case EASY:
